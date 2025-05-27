050ba4bbe64c20f0d81c5f520d9cb236b5722041
vipermr <vipermr492@gmail.com>
2025-05-27 12:02:36 +0600

    Update pp.js

--- scripts/cmds/pp.js
+++ scripts/cmds/pp.js
@@ -1,57 +1,33 @@
-const axios = require("axios");
-
 module.exports = {
   config: {
-    name: "profile",
-    aliases: ["pp"],
-    version: "1.3",
-    author: "NAFIJ_PRO( MODED )",
+    name: "pp",
+    aliases: ["pfp", "profile"],
+    version: "1.1",
+    author: "dipto",
     countDown: 5,
     role: 0,
-    shortDescription: "Get Facebook profile picture",
-    longDescription: "Fetch profile picture via tag, reply, self or facebook.com link",
+    description: "PROFILE image",
     category: "image",
-    guide: {
-      en: "{pn} @tag\n{pn} (reply to message)\n{pn} (your own profile)\n{pn} facebook.com/username"
-    }
+    guide: { en: "{pn} @tag or userID or reply to a message or provide a Facebook URL" }
   },
-
   onStart: async function ({ event, message, usersData, args }) {
-    let targetID;
-
-    // Facebook link input
-    if (args[0] && args[0].includes("facebook.com")) {
-      try {
-        const res = await axios.get(`https://graph.facebook.com/v18.0/${args[0].split("facebook.com/")[1]}?fields=id&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`);
-        targetID = res.data.id;
-      } catch (err) {
-        return message.reply("Failed to fetch user from Facebook link. Make sure the profile is public.");
-      }
-    }
-
-    // If reply
-    else if (event.type === "message_reply") {
-      targetID = event.messageReply.senderID;
-    }
-
-    // If tag
-    else if (Object.keys(event.mentions).length > 0) {
-      targetID = Object.keys(event.mentions)[0];
-    }
-
-    // Default to self
-    else {
-      targetID = event.senderID;
-    }
+    const getAvatarUrl = async (uid) => await usersData.getAvatarUrl(uid);
+    const uid = Object.keys(event.mentions)[0] || args[0] || event.senderID;
+    let avt;
 
     try {
-      const avatarURL = `https://graph.facebook.com/${targetID}/picture?width=720&height=720`;
-      return message.reply({
-        body: "Here is the profile picture:",
-        attachment: await global.utils.getStreamFromURL(avatarURL)
-      });
-    } catch (e) {
-      return message.reply("Error getting profile picture. Please try again.");
+      if (event.type === "message_reply") {
+        avt = await getAvatarUrl(event.messageReply.senderID);
+      } else if (args.join(" ").includes("facebook.com")) {
+        const match = args.join(" ").match(/(\d+)/);
+        if (match) avt = await getAvatarUrl(match[0]);
+        else throw new Error("Invalid Facebook URL.");
+      } else {
+        avt = await getAvatarUrl(uid);
+      }
+      message.reply({ body: "", attachment: await global.utils.getStreamFromURL(avt) });
+    } catch (error) {
+      message.reply(`⚠️ Error: ${error.message}`);
     }
   }
 };
